87a45545eaa949fa89505264282d123a7e303705
Salami Oluwadamilola <105321247+TiminiOfLagos@users.noreply.github.com>
2023-08-14 22:23:13 +0100

    Add files via upload

---
+++ src/Pages/Signin.jsx
@@ -0,0 +1,126 @@
+import React from 'react'
+import {useState} from 'react'
+import logo from "../Assets/Sail logo.png"
+import { Link } from 'react-router-dom'
+
+const Signin = () => {
+  
+  //This state holds the data entered by the user in the form fields and updates it
+  const [formInfo, setFormInfo] = useState({
+    email: '',
+    password: '',
+  });
+ 
+  //This state holds the error messages and allows the display of it when there issues with the form inputs
+  const [errors, setErrors] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormInfo((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const validateForm = () => {
+    let newErrors = {};
+    if (!formInfo.email) {
+      newErrors.email = 'Email is required';
+    }
+    if (!formInfo.password) {
+      newErrors.password = 'Password is required';
+    }
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (validateForm()) {
+      // Form is valid, you can proceed with form submission or other actions
+      console.log('Form submitted with data:', formInfo);
+    } else {
+      console.log('Form has errors');
+    }
+  };
+
+
+  return (
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 h-screen bg-gray-800">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            className="mx-auto h-10 w-auto"
+            src={logo}
+            alt="Your Company"
+          />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">
+            Sign in to your account
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" method="POST" onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-white">
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  onChange={handleInputChange}
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+                    {errors.email && <p className="text-red-500">{errors.email}</p>}
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-white">
+                  Password
+                </label>
+                <div className="text-sm">
+                  <a href="" className="font-semibold text-blue-600 hover:text-indigo-500">
+                    Forgot password?
+                  </a>
+                </div>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  onChange={handleInputChange}
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+                {errors.password && <p className="text-red-500">{errors.password}</p>}
+              </div>
+            </div>
+
+            <div>
+                <Link to= "/landing">
+                <button
+                type="submit"
+                className="flex w-full justify-center rounded-md  bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign in
+              </button>
+                </Link>
+            </div>
+          </form>
+        </div>
+      </div>
+
+  )
+}
+
+export default Signin
